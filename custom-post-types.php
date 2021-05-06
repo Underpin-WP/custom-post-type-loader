15aa239c9a6bd156f1b4672ff014a735400d02e4
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:15:22 -0700

    Corrects underpin autoloader

--- custom-post-types.php
+++ custom-post-types.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Custom_Post_Type.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Custom_Post_Type_Instance.php' );
-		Underpin\underpin()->get( $file )->loaders()->add( 'custom_post_types', [
+		Underpin\underpin()->get( $file, $class )->loaders()->add( 'custom_post_types', [
 			'instance' => 'Underpin_Custom_Post_Types\Abstracts\Custom_Post_Type',
 			'default'  => 'Underpin_Custom_Post_Types\Factories\Custom_Post_Type_Instance'
 		] );
-} );
+}, 10, 2 );
