72923fe7942566fb032f14ace40ba1934713dee0
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:04:04 -0600

    Compat for Underpin 2.0.0

--- custom-post-types.php
+++ custom-post-types.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'custom_post_types', [
+	'update' => function ( Underpin $plugin ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Custom_Post_Type.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Custom_Post_Type_Instance.php' );
-		Underpin\underpin()->get( $file, $class )->loaders()->add( 'custom_post_types', [
+		$plugin->loaders()->add( 'custom_post_types', [
 			'instance' => 'Underpin_Custom_Post_Types\Abstracts\Custom_Post_Type',
 			'default'  => 'Underpin_Custom_Post_Types\Factories\Custom_Post_Type_Instance'
 		] );
-}, 10, 2 );
+	},
+] ) );
