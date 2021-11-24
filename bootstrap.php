7c304590a4a4b794e7b51b971d072280399d78b2
Alex Standiford <a@alexstandiford.com>
2021-11-24 15:33:04 -0600

    Fixes loader

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 Underpin::attach( 'setup', new \Underpin\Factories\Observers\Loader( 'custom_post_types', [
-	'instance' => 'Underpin\Custom_Post_Types\Abstracts\Custom_Post_Type',
-	'default'  => 'Underpin\Custom_Post_Types\Factories\Custom_Post_Type_Instance',
+	'abstraction_class' => 'Underpin\Custom_Post_Types\Abstracts\Custom_Post_Type',
+	'default_factory'  => 'Underpin\Custom_Post_Types\Factories\Custom_Post_Type_Instance',
 ] ) );
